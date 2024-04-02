4a0fc6e684d7f77e794f39216e6d207e1373bf32
mareme <mariamathiaw92@gmail.com>
2024-04-02 23:08:57 +0000

    Implémentation du fichier qui permet d'ajouter des membres de la commune

---
+++ adddata.php
@@ -0,0 +1,21 @@
+<?php
+//inclusion du fichier de connexion
+require_once "config.php"; 
+//verifier si le formulaire a été soumis
+if(isset($_POST['submit'])){
+    //recuperation des données
+    $matricule= $_POST['matricule'];
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $tranche_age= $_POST['tranche_age'];
+    $sexe = $_POST['sexe'];
+    $siuation_matrimoniale = $_POST['siuation_matrimoniale'];
+    $statut = $_POST['statut'];
+
+    //verifier si les champs ne sont pas vide 
+    if($matricule !="" && $nom !="" && $prenom !="" && $tranche_age !="" && $sexe !="" && $situation_matrimoniale !="" && $statut !=""){
+        //appel de la methode 
+        $membre->addMembres($matricule, $nom, $prenom, $tranche_age, $sexe, $situation_matrimoniale, $statut);
+    }
+}
+?>
