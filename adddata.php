35aa9b57a3e978fb478af2907ec69b777b9c53de
mareme <mariamathiaw92@gmail.com>
2024-04-06 22:21:29 +0000

    Adaptation du fichier adddata pour la 2e partie de l'atelier

--- adddata.php
+++ adddata.php
@@ -5,23 +5,34 @@ error_reporting(E_ALL);
 
 //inclusion du fichier de connexion
 require_once "config.php"; 
+
+// Script pour récupérer les données de la table statut
+$sql_statut = "SELECT id, libelle_statut FROM statut";
+$stmt_statut = $connexion->query($sql_statut);
+$liste_statut = $stmt_statut->fetchAll(PDO::FETCH_ASSOC);
+
+// Script pour récupérer les données de la table tranche_age
+$sql_tranche_age = "SELECT id, libelle_tranche_age FROM tranche_age";
+$stmt_tranche_age = $connexion->query($sql_tranche_age);
+$liste_tranche_age = $stmt_tranche_age->fetchAll(PDO::FETCH_ASSOC);
+
 //verifier si le formulaire a été soumis
 if(isset($_POST['submit'])){
 
-    // var_dump($_POST);
     //recuperation des données
-    $matricule= $_POST['matricule'];
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
-    $tranche_age= $_POST['tranche_age'];
     $sexe = $_POST['sexe'];
     $situation_matrimoniale = $_POST['situation_matrimoniale'];
-    $statut = $_POST['statut'];
+
+    // Récupération des valeurs sélectionnées du statut et de la tranche d'âge
+    $id_tranche_age = $_POST['tranche_age'];
+    $id_statut = $_POST['statut'];
 
     //verifier si les champs ne sont pas vide 
-    if($matricule !="" && $nom !="" && $prenom !="" && $tranche_age !="" && $sexe !="" && $situation_matrimoniale !="" && $statut !=""){
+    if($nom !="" && $prenom !="" && $id_tranche_age !="" && $sexe !="" && $situation_matrimoniale !="" && $id_statut !=""){
         //appel de la methode 
-        $membre->addMembres($matricule, $nom, $prenom, $tranche_age, $sexe, $situation_matrimoniale, $statut);
+        $membre->addMembres($nom, $prenom, $sexe, $situation_matrimoniale, $id_tranche_age, $id_statut);
     }
 }
 ?>
